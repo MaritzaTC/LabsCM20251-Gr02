8890559a29c8e588bcb1d9848ab6b44f76dc94af
MaritzaTC <maritza.tabarezc@udea.edu.co>
2025-04-16 23:09:56 -0500

    fix(🎨): Add missing imports and annotation for API level requirement in MainActivity

--- Lab120251-Gr02/Lab1-UI/src/main/java/co/edu/udea/compumovil/gr02_20251/labs20251_gr02/MainActivity.kt
+++ Lab120251-Gr02/Lab1-UI/src/main/java/co/edu/udea/compumovil/gr02_20251/labs20251_gr02/MainActivity.kt
@@ -1,9 +1,11 @@
 package co.edu.udea.compumovil.gr02_20251.labs20251_gr02
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -16,6 +18,7 @@ import co.edu.udea.compumovil.gr02_20251.labs20251_gr02.presentation.templates.P
 import co.edu.udea.compumovil.gr02_20251.labs20251_gr02.presentation.tokens.Labs20251Gr02Theme
 
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -28,7 +31,6 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
-
         }}
 
 

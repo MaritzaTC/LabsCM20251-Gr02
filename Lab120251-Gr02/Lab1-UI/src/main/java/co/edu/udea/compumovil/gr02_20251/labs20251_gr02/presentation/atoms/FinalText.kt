55119326a3e1fe3628f6a454fd80c5991e15ade8
MaritzaTC <maritza.tabarezc@udea.edu.co>
2025-04-17 14:32:55 -0500

    feat(✨): Add FinalText, FinalCard, and FinalForm composables for enhanced UI structure and layout

---
+++ Lab120251-Gr02/Lab1-UI/src/main/java/co/edu/udea/compumovil/gr02_20251/labs20251_gr02/presentation/atoms/FinalText.kt
@@ -0,0 +1,22 @@
+package co.edu.udea.compumovil.gr02_20251.labs20251_gr02.presentation.atoms
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+
+@Composable
+fun FinalText(text: String) {
+    Text(
+        text = text,
+        modifier = Modifier
+            .padding(16.dp),
+        textAlign = TextAlign.Center,
+    )
+}
---
+++ Lab120251-Gr02/Lab1-UI/src/main/java/co/edu/udea/compumovil/gr02_20251/labs20251_gr02/presentation/templates/FinalForm.kt
@@ -0,0 +1,29 @@
+package co.edu.udea.compumovil.gr02_20251.labs20251_gr02.presentation.templates
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import co.edu.udea.compumovil.gr02_20251.labs20251_gr02.presentation.atoms.Logo
+import co.edu.udea.compumovil.gr02_20251.labs20251_gr02.presentation.atoms.SimpleNavbar
+import co.edu.udea.compumovil.gr02_20251.labs20251_gr02.presentation.molecules.FinalCard
+
+@Composable
+fun FinalForm() {
+    Box(  modifier = Modifier
+        .fillMaxSize(),) {
+        Column(
+        ) {
+            SimpleNavbar()
+            Logo()
+            Column(
+                modifier = Modifier
+                    .padding(start = 70.dp, top = 16.dp)
+            ) {
+                FinalCard()
+            }
+        }
+    }}

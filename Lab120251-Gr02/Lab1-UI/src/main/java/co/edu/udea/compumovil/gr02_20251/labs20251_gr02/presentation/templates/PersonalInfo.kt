8094d6a581fc7ca0448145043593edebe2899176
MaritzaTC <maritza.tabarezc@udea.edu.co>
2025-04-16 23:09:30 -0500

    refactor(✨): Restructure PersonalInfo composable layout and improve readability

--- Lab120251-Gr02/Lab1-UI/src/main/java/co/edu/udea/compumovil/gr02_20251/labs20251_gr02/presentation/templates/PersonalInfo.kt
+++ Lab120251-Gr02/Lab1-UI/src/main/java/co/edu/udea/compumovil/gr02_20251/labs20251_gr02/presentation/templates/PersonalInfo.kt
@@ -1,21 +1,47 @@
 package co.edu.udea.compumovil.gr02_20251.labs20251_gr02.presentation.templates
 
+import android.os.Build
+import androidx.annotation.RequiresApi
+
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+
 import androidx.compose.foundation.layout.fillMaxSize
+
+import androidx.compose.foundation.layout.padding
+
 import androidx.compose.runtime.Composable
+
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import co.edu.udea.compumovil.gr02_20251.labs20251_gr02.presentation.atoms.Logo
+
 import co.edu.udea.compumovil.gr02_20251.labs20251_gr02.presentation.atoms.SimpleNavbar
 import co.edu.udea.compumovil.gr02_20251.labs20251_gr02.presentation.atoms.TitleText
 
+import co.edu.udea.compumovil.gr02_20251.labs20251_gr02.presentation.molecules.IconInput
+
+import co.edu.udea.compumovil.gr02_20251.labs20251_gr02.presentation.organisms.RadioGroupAndIcon
+
+@RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun PersonalInfo(){
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-    ) {
-        SimpleNavbar()
-
-        TitleText(text = "Información Personal")
+  Box(  modifier = Modifier
+      .fillMaxSize(),){
+      Column(
+  ) {    SimpleNavbar()
+          Logo()
+          TitleText(text = "Información personal")
+          Column(
+              modifier = Modifier
+              .padding(start = 70.dp, top = 16.dp)
+          ) {
+        IconInput("Nombres:")
+        IconInput("Apellidos:")
+              RadioGroupAndIcon()
 
     }
+  }
+
+  }
 }

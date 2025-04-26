e39bd25574ad16c12250835f26b90b88c35b0d2a
MaritzaTC <maritza.tabarezc@udea.edu.co>
2025-04-26 02:36:31 -0500

    feat: add FinalFormLandscape component with navigation and layout

--- Lab120251-Gr02/Lab1-UI/src/main/java/co/edu/udea/compumovil/gr02_20251/labs20251_gr02/presentation/templates/FinalFormLandscape.kt
+++ Lab120251-Gr02/Lab1-UI/src/main/java/co/edu/udea/compumovil/gr02_20251/labs20251_gr02/presentation/templates/FinalFormLandscape.kt
@@ -2,12 +2,12 @@ package co.edu.udea.compumovil.gr02_20251.labs20251_gr02.presentation.templates
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-
 import androidx.navigation.NavController
 import co.edu.udea.compumovil.gr02_20251.labs20251_gr02.presentation.atoms.Logo
 import co.edu.udea.compumovil.gr02_20251.labs20251_gr02.presentation.atoms.SimpleNavbar
@@ -16,7 +16,7 @@ import co.edu.udea.compumovil.gr02_20251.labs20251_gr02.presentation.organisms.F
 import co.edu.udea.compumovil.gr02_20251.labs20251_gr02.presentation.organisms.UserInfoViewModel
 
 @Composable
-fun FinalForm(viewModel: UserInfoViewModel,  navController: NavController) {
+fun FinalFormLandscape(viewModel: UserInfoViewModel,  navController: NavController) {
     Box(  modifier = Modifier
         .fillMaxSize(),) {
         Column(
@@ -27,7 +27,8 @@ fun FinalForm(viewModel: UserInfoViewModel,  navController: NavController) {
                 modifier = Modifier
                     .padding(start = 70.dp, top = 16.dp)
             ) {
-                FinalCard(navController = navController)
+                FinalCardLandscape(navController = navController)
             }
         }
     }}
+

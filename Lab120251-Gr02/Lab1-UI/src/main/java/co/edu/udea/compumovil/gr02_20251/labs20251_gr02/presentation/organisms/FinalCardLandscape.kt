90eb7126b193ddcdffcfe45201d84aea95af0faa
MaritzaTC <maritza.tabarezc@udea.edu.co>
2025-04-26 02:34:52 -0500

    feat: refactor FinalCard package and add FinalCardLandscape component

--- Lab120251-Gr02/Lab1-UI/src/main/java/co/edu/udea/compumovil/gr02_20251/labs20251_gr02/presentation/organisms/FinalCardLandscape.kt
+++ Lab120251-Gr02/Lab1-UI/src/main/java/co/edu/udea/compumovil/gr02_20251/labs20251_gr02/presentation/organisms/FinalCardLandscape.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -29,44 +30,56 @@ import co.edu.udea.compumovil.gr02_20251.labs20251_gr02.presentation.tokens.Gree
 import co.edu.udea.compumovil.gr02_20251.labs20251_gr02.presentation.tokens.MintGreen
 
 @Composable
-fun FinalCard(navController: NavController){
+fun FinalCardLandscape(navController: NavController){
     OutlinedCard(
         colors = CardDefaults.cardColors(
             containerColor = MaterialTheme.colorScheme.surface,
         ),
         border = BorderStroke(1.dp,MintGreen),
         modifier = Modifier
-            .size(width = 280.dp, height = 450.dp)
+            .size(width = 800.dp, height = 280.dp)
 
     ) {
-        FinalText(stringResource(id = R.string.final_form))
-        Column(   modifier = Modifier
-            .fillMaxWidth()
-            .padding(26.dp)) {
-            ImageLoader()
-        }
+        Row {
+            Column( modifier = Modifier
+                .weight(1f)
+                .padding(start = 2.dp, top = 50.dp)) {
 
-        Box(
-            modifier = Modifier
-                .padding(35.dp)
-                .fillMaxWidth()
-                .background(GreenToBlueGradient, shape = RoundedCornerShape(15.dp)),
-        )
-        {
-            Button(onClick = { navController.navigate("personal") },
-                modifier = Modifier
-                    .align(Alignment.Center)
-                    .size(width = 207.dp, height = 44.dp),
+                FinalText(stringResource(id = R.string.final_form))
+            }
+            Column ( modifier = Modifier
+                .weight(1f)){
+                ImageLoader()
+            }
+            Column(modifier = Modifier
+                .weight(1f)) {
+                Box(
+                    modifier = Modifier
+                        .padding(top = 60.dp)
+                        .size(width = 180.dp, height = 44.dp)
+                        .background(GreenToBlueGradient, shape = RoundedCornerShape(15.dp)),
+                )
+                {
+                    Button(onClick = { navController.navigate("personal") },
+                        modifier = Modifier
+                            .align(Alignment.Center)
+                            .size(width = 180.dp, height = 44.dp)                        ,
 
-                colors = ButtonDefaults.buttonColors(
-                    containerColor = Color.Transparent,
-                    contentColor = Color.Black
-                )
-            ) {
-                Text(stringResource(id = R.string.final_button),
-                )
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = Color.Transparent,
+                            contentColor = Color.Black
+                        )
+                    ) {
+                        Text(stringResource(id = R.string.final_button),
+                        )
+                    }
+                }
             }
         }
+
+
+
+
 
     }
 }

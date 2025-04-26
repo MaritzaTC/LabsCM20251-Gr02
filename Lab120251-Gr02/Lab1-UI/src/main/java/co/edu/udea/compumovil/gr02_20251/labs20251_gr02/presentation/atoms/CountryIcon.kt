beaf4a70427b283e52fb6e940febda10cc2c3a54
GuillermoSanchez11 <guillermo.sanchez1@udea.edu.co>
2025-04-25 23:02:03 -0500

    feat: add countryIcon composable for country selection input

---
+++ Lab120251-Gr02/Lab1-UI/src/main/java/co/edu/udea/compumovil/gr02_20251/labs20251_gr02/presentation/atoms/CountryIcon.kt
@@ -0,0 +1,23 @@
+package co.edu.udea.compumovil.gr02_20251.labs20251_gr02.presentation.atoms
+
+import androidx.compose.foundation.Image
+
+import androidx.compose.foundation.layout.size
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import co.edu.udea.compumovil.gr02_20251.labs20251_gr02.R
+
+
+@Composable
+fun CountryIcon(){
+    val image = painterResource(id= R.drawable.property_1_variant3)
+    Image(
+        painter = image,
+        contentDescription ="country",
+        modifier = Modifier
+            .size(24.dp)
+    )
+}
